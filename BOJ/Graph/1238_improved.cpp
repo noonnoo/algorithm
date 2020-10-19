3a3aa4b88528fb837be951ca0c2689a3ad9e6e8c
눈누 <33820372+noonnoo@users.noreply.github.com>
2020-10-19 23:23:14 +0900

    코드 줄이는 업뎃

--- BOJ/Graph/1238_improved.cpp
+++ BOJ/Graph/1238_improved.cpp
@@ -4,12 +4,10 @@
 #define INF 10000000
 using namespace std;
 
-vector<pair<int, int>> road[1001];
-vector<pair<int, int>> r_road[1001];
-vector<int> x_dist, x_dist_r;
-int N, M, X;
+vector<int> x_dist;
+int N, M, X, largest = 0;
 
-void dijkstra(int s, bool r) {
+void dijkstra(int s, bool r,const vector<vector<pair<int, int>>> &graph) {
 	vector<int> dist(N + 1, INF);
 	dist[s] = 0;
 
@@ -20,47 +18,37 @@ void dijkstra(int s, bool r) {
 		pair<int, int> curr = heap.top();
 		heap.pop();
 
-		if(r)
-			for (auto next : road[curr.second]) {		//next.first: 현재에서 가려는 노드, next.second: 현재에서 first까지 가는데 걸리는 시간
-				int next_t = curr.first + next.second;	//현재까지오는데 time + 다음번거 가는데 걸리는 time
-				if (next_t < dist[next.first]) {
-					dist[next.first] = next_t;
-					heap.push(make_pair(next_t, next.first));
-				}
+		for (auto next : graph[curr.second]) {		//next.first: 현재에서 가려는 노드, next.second: 현재에서 first까지 가는데 걸리는 시간
+			int next_t = curr.first + next.second;	//현재까지오는데 time + 다음번거 가는데 걸리는 time
+			if (next_t < dist[next.first]) {
+				dist[next.first] = next_t;
+				heap.push(make_pair(next_t, next.first));
 			}
-		else
-			for (auto next : r_road[curr.second]) {		//next.first: 현재에서 가려는 노드, next.second: 현재에서 first까지 가는데 걸리는 시간
-				int next_t = curr.first + next.second;	//현재까지오는데 time + 다음번거 가는데 걸리는 time
-				if (next_t < dist[next.first]) {
-					dist[next.first] = next_t;
-					heap.push(make_pair(next_t, next.first));
-				}
-			}
+		}
 	}
 
-	if (r)
+	if (!r)
 		x_dist = dist;
 	else
-		x_dist_r = dist;
+		for (int i = 1; i <= N; i++) {
+			if (largest < x_dist[i] + dist[i])
+				largest = x_dist[i] + dist[i];
+		}
 }
 
 int main() {
-	int ct1, ct2, time, largest = 0;
+	int ct1, ct2, time;
 	cin >> N >> M >> X;
-	
+	vector<vector<pair<int, int>>> road(N + 1), r_road(N + 1);
+
 	for (int i = 0; i < M; i++) {
 		cin >> ct1 >> ct2 >> time;
 		road[ct1].push_back(make_pair(ct2, time));
 		r_road[ct2].push_back(make_pair(ct1, time));
 	}
 
-	dijkstra(X, true);
-	dijkstra(X, false);
-
-	for (int i = 1; i <= N; i++) {
-		if (largest < x_dist[i] + x_dist_r[i])
-			largest = x_dist[i] + x_dist_r[i];
-	}
+	dijkstra(X, false, road);
+	dijkstra(X, true, r_road);
 
 	cout << largest;
 }
