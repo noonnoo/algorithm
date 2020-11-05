be018f5d0dc1415ddd6cf945743d5fad4514605d
눈누 <33820372+noonnoo@users.noreply.github.com>
2020-11-05 23:36:51 +0900

    Update card_game.cpp

--- Programmers/DP/card_game.cpp
+++ Programmers/DP/card_game.cpp
@@ -1,15 +1,36 @@
-int solve(int l, int r, int score) {
-    if (l >= left_g.size() || r >= right_g.size())  //카드를 다 쓴 경우
-        return score;
-    
-    if (left_g[l] > right_g[r]) {       //오른쪽이 더 작은 경우
-        score += solve(l, r + 1, score) + right_g[r];
-        return score;
-    } else {                            //아닌 경우
-        int temp1 = solve(l + 1, r + 1, score);
-        int temp2 = solve(l + 1, r, score);
+#include <string>
+#include <vector>
+#include <iostream>
 
-        score += temp1 >= temp2 ? temp1 : temp2;    //둘다 끝까지 가보고 비교
-        return score;
+using namespace std;
+
+int solution(vector<int> left, vector<int> right) {
+    int answer = 0, n = left.size();
+
+    int** score = new int*[n + 1];
+    for (int i = 0; i < n + 1; i++) {
+        score[i] = new int[n + 1];
+        memset(score[i], 0, sizeof(int) * (n + 1));
     }
+
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = n - 1; j >= 0; j--) {
+            if (right[i] < left[j]) {
+                score[i][j] = score[i + 1][j] + right[i];
+            } else {
+                score[i][j] = score[i + 1][j + 1] >= score[i][j + 1] ? score[i + 1][j + 1] : score[i][j + 1];
+            }
+        }
+    }
+    answer = score[0][0];
+
+    for (int i = 0; i < n + 1; i++)
+        delete[] score[i];
+    delete[] score;
+
+    return answer;
+}
+
+int main() {
+    cout << solution({ 3,2,5 }, { 2,4,1 });
 }
