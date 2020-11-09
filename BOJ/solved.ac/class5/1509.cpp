2e17bfa0b4091b3d8978e2874c3ffbde3a051c1a
눈누 <33820372+noonnoo@users.noreply.github.com>
2020-11-09 22:52:32 +0900

    Create 1509.cpp

---
+++ BOJ/solved.ac/class5/1509.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string str;
+bool DP[2501][2501];
+int cnt[2501];
+
+void get_pelidrome(int &size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < i + 2; j++) {
+			if (i <= j)
+				DP[i][j] = 1;
+			else if (DP[i - 1][j + 1] == true && str[i] == str[j])
+				DP[i][j] = 1;
+			else
+				DP[i][j] = 0;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0);
+	cin >> str;
+	int size = str.size();
+
+	get_pelidrome(size);
+
+	cnt[0] = 1;
+ 	for (int i = 1; i < size; i++) {
+		cnt[i] = 1e9;
+		for (int j = 0; j < i + 1; j++) {
+			if (DP[i][j]) {
+				if (j == 0 && cnt[i] > 1)
+					cnt[i] = 1;
+				else if (cnt[i] > cnt[j - 1] + 1)
+					cnt[i] = cnt[j - 1] + 1;
+			}
+		}
+	}
+
+	cout << cnt[size - 1];
+}
